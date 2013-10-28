ed4d8e0b6e75810146ae58ed151f476019f78ae9
jcockfie <jeremy.cockfield@gmail.com>
2013-10-28 13:36:53 -0500

    added unit test file for CSV modality

---
+++ mobbedDB/mobbed_tests/testCSV.m
@@ -0,0 +1,30 @@
+function test_suite = testGENERIC %#ok<STOUT>
+initTestSuite;
+
+function tStruct = setup %#ok<DEFNU>
+tStruct = struct('name', 'testdb', 'url', 'localhost', ...
+    'user', 'postgres', 'password', 'admin', 'DB', []);
+try
+    DB = Mobbed(tStruct.name, tStruct.url, tStruct.user, ...
+        tStruct.password, false);
+catch ME %#ok<NASGU>
+    Mobbed.createdb(tStruct.name, tStruct.url, tStruct.user, ...
+        tStruct.password, 'mobbed.sql', false);
+    DB = Mobbed(tStruct.name, tStruct.url, tStruct.user, ...
+        tStruct.password, false);
+end
+tStruct.DB = DB;
+
+m = getdb(DB, 'modalities', inf);
+mNames = {m.modality_name};
+pos = strcmp('CSV', mNames);
+uuids = {m.modality_uuid};
+tStruct.mUUID = uuids{pos};
+
+% Function executed after each test
+function teardown(~) %#ok<DEFNU>
+try
+    Mobbed.closeall();
+catch ME %#ok<NASGU>
+end
+
